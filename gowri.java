513674e63b2f80ecd407b050217f4b3503850b5e
srigowri <srigowri@gmail.com>
2022-12-23 11:21:50 +0530

    sd

---
+++ gowri.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+class gowri{
+	public static void main (String[] args){
+		Scanner chinni=new Scanner(System.in);
+		System.out.print("enter your name:\t");
+		String gowri=chinni.nextLine();
+		System.out.print("Your Name is :-->\t" +gowri);
+		
+		
+		
+		
+	}
+}
