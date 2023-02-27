c883aac31b98dc3af413181a9234d077b901b713
srigowrikonati <120468308+srigowrikonati@users.noreply.github.com>
2023-02-27 15:06:02 +0530

    Update gowri.java

--- gowri.java
+++ gowri.java
@@ -2,10 +2,8 @@ import java.util.*;
 import java.io.*;
 class gowri{
 	public static void main (String[] args){
-		Scanner chinni=new Scanner(System.in);
-		System.out.print("enter your name:\t");
-		String gowri=chinni.nextLine();
-		System.out.print("Your Name is :-->\t" +gowri);
+
+		System.out.print("Your Name is :-->");
 		
 		
 		
